401d39d7f24c86a80347e45ae59f23c5aa9fcaa5
Naretto95 <narettolilian@hotmail.fr>
2022-10-05 00:46:26 +0900

    Add responsive sidebar navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,62 @@
 jQuery(document).ready(function() {
+	const select = (el, all = false) => {
+		el = el.trim()
+		if (all) {
+		  return [...document.querySelectorAll(el)]
+		} else {
+		  return document.querySelector(el)
+		}
+	  }
 	
-	/*
-	    Sidebar
-	*/
-	$('.dismiss').on('click', function() {
-        $('.sidebar').removeClass('active');
-	});
-
-	$('.sidebar .scroll-link').click(function() {
-		if(window.matchMedia("(max-width: 992px)").matches){
-			$(".sidebar").removeClass("active");
-			$(".scroll-link").removeClass("active");
-		};
-	});
-
-
-    $('.open-menu').on('click', function(e) {
-    	e.preventDefault();
-        $('.sidebar').addClass('active');
-        // close opened sub-menus
-        $('.collapse.show').toggleClass('show');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-    /* change sidebar style */
-	$('a.btn-customized-dark').on('click', function(e) {
-		e.preventDefault();
-		$('.sidebar').removeClass('light');
-	});
-	$('a.btn-customized-light').on('click', function(e) {
-		e.preventDefault();
-		$('.sidebar').addClass('light');
-	});
+	  /**
+	   * Easy event listener function
+	   */
+	  const on = (type, el, listener, all = false) => {
+		let selectEl = select(el, all)
+		if (selectEl) {
+		  if (all) {
+			selectEl.forEach(e => e.addEventListener(type, listener))
+		  } else {
+			selectEl.addEventListener(type, listener)
+		  }
+		}
+	  }
+	
+	  /**
+	   * Easy on scroll event listener 
+	   */
+	  const onscroll = (el, listener) => {
+		el.addEventListener('scroll', listener)
+	  }
+	
+	  /**
+	   * Navbar links active state on scroll
+	   */
+	  let navbarlinks = select('#navbar .scrollto', true)
+	  const navbarlinksActive = () => {
+		let position = window.scrollY + 200
+		navbarlinks.forEach(navbarlink => {
+		  if (!navbarlink.hash) return
+		  let section = select(navbarlink.hash)
+		  if (!section) return
+		  if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+			navbarlink.classList.add('active')
+		  } else {
+			navbarlink.classList.remove('active')
+		  }
+		})
+	  }
+	  window.addEventListener('load', navbarlinksActive)
+	  onscroll(document, navbarlinksActive)
+	
+	  /**
+	   * Mobile nav toggle
+	   */
+	  on('click', '.mobile-nav-toggle', function(e) {
+		select('body').classList.toggle('mobile-nav-active')
+		this.classList.toggle('bi-list')
+		this.classList.toggle('bi-x')
+	  })
 	
 	// Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
@@ -55,26 +81,30 @@ jQuery(document).ready(function() {
 		  $('.scroll-to-top').fadeOut();
 		}
 	  });
-});
 
-var $trigger =$('.skillbar');
+	  window.addEventListener('load', () => {
+		AOS.init({
+		  duration: 1000,
+		  easing: 'ease-in-out',
+		  once: true,
+		  mirror: false
+		})
+	  });
 
-if(window.matchMedia("(max-width: 992px)").matches){
-	$(".wow").removeClass("wow");
-	$(".animated").removeClass("animated");
-};
+	  var $trigger =$('.skillbar');
 
-$trigger.waypoint(function(){
-	jQuery(document).ready(function(){
-		jQuery('.skillbar').each(function(){
-			jQuery(this).find('.skillbar-bar').animate({
-				width:jQuery(this).attr('data-percent')
-			},6000);
+	$trigger.waypoint(function(){
+		jQuery(document).ready(function(){
+			jQuery('.skillbar').each(function(){
+				jQuery(this).find('.skillbar-bar').animate({
+					width:jQuery(this).attr('data-percent')
+				},6000);
+			});
 		});
-	});
-},{offset:'100%'})
+	},{offset:'100%'})
 
-window.onload = function(){
-	//hide the preloader
-	document.querySelector("#preloader").style.display = "none";
-}
+	window.onload = function(){
+		//hide the preloader
+		document.querySelector("#preloader").style.display = "none";
+	}
+});
